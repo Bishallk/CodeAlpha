14a81822a14936d371707aa69feb8060ab215b4d
Bishallk <3276617@mbmc.edu.np>
2025-02-24 17:17:09 +0545

    fibonacci series using generator

--- CodeAlpha_Fibonacci_Generator.py
+++ CodeAlpha_Fibonacci_Generator.py
@@ -2,17 +2,40 @@
 # #-The Fibonacci series is a sequence where each number is the sum of the two preceding numbers, defined by a mathematical recurrence relationship.
 
 
+def get_input():
+    return int(input("Enter Number of Fibonacci : ")) #taking input from users.
+
 def fibo_generator():
-    n=int(input("Enter Number of Fibonacci : ")) #taking input from users.
+    n=get_input()
     series=[] #list to store fibonacci series
     if(n<0):
         print("Please Enter Positive Number.")
+        return
     else:
         a,b=0,1
         for _ in range(n):
             series.append(a) #adding new fibonacci to list
             a,b=b,a+b
-        
-    return series
+    return series #returns list of fibonacci series
 
-print(fibo_generator()) #
+print("Normal: ",fibo_generator())
+
+
+#using Generator / yield keyword
+def fibonacci():
+    n=get_input()
+    if(n>0):
+        a,b=0,1
+        for _ in range(n):
+            yield a
+            a,b=b,a+b
+    else:
+        print("Please Enter Positive Number")
+        return
+
+series= fibonacci()
+print("using generator")
+for i in series:
+    print(i)
+
+
